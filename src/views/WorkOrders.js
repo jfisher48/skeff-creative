27b2c3811832273956d52686ac8949dc32d6d649
Jay Fisher <jmfisher@outlook.com>
2018-11-08 15:22:43 -0600

    Loan Reminders Nav to Workorders and add button to create WO

--- src/views/WorkOrders.js
+++ src/views/WorkOrders.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import PageHeading from "../components/PageHeading.js";
-import DashboardIco from "../icons/dashboard_b.svg";
+import RemindersIco from "../icons/reminders_b.svg";
 import Helmet from "react-helmet";
 import CreateWorkOrder from "../components/workorders/CreateWorkOrder.js";
-import { Route, Switch } from "react-router-dom";
-import { Grid } from "@material-ui/core";
+import { Switch, Link } from "react-router-dom";
+import { Grid, Button, Typography } from "@material-ui/core";
 import WorkOrderList from "../components/workorders/WorkOrderList.js";
 import WorkOrderDetail from "../components/workorders/WorkOrderDetail.js";
 import { connect } from "react-redux";
@@ -22,27 +22,43 @@ class WorkOrders extends Component {
         <Helmet>
           <title>Work Orders | Skeff Creative Services</title>
         </Helmet>
-        <PageHeading headingIcon={DashboardIco}>Work Orders</PageHeading>
+        <PageHeading headingIcon={RemindersIco}>Work Orders</PageHeading>
         <Grid container spacing={16}>
           <Grid item xs={12} lg={8}>
-            <WorkOrderList workorders={workorders} />
-            {/* <Switch> */}
-            {/* <Route
+            <Grid container spacing={16}>
+              <Grid item xs={12}>
+                <Link to="workorders/create">
+                  <Button
+                    style={{ float: "right" }}
+                    variant="outlined"
+                    size="large"
+                    color="secondary"
+                  >
+                    New Order
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item xs={12}>
+                <WorkOrderList workorders={workorders} />
+              </Grid>
+              {/* <Switch> */}
+              {/* <Route
               path="/workorders"
               render={() => <WorkOrderList workorders={workorders} />}
             /> */}
-            <Switch>
-              <ModalRoute
-                path="/workorders/create"
-                parentPath="/workorders"
-                component={CreateWorkOrder}
-              />
-              <ModalRoute
-                path="/workorders/:id"
-                parentPath="/workorders"
-                component={WorkOrderDetail}
-              />
-            </Switch>
+              <Switch>
+                <ModalRoute
+                  path="/workorders/create"
+                  parentPath="/workorders"
+                  component={CreateWorkOrder}
+                />
+                <ModalRoute
+                  path="/workorders/:id"
+                  parentPath="/workorders"
+                  component={WorkOrderDetail}
+                />
+              </Switch>
+            </Grid>
           </Grid>
           <Grid item xs={12} lg={4} />
           <ModalContainer />
